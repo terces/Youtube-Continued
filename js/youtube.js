83ad805ad8cdf848c4bd630ee1c3dd64a71a858c
Terces Tsai <nonameds@gmail.com>
2013-01-14 21:52:34 +0800

    terces: fixed history 'play' bug, and modified the related solution

--- js/youtube.js
+++ js/youtube.js
@@ -166,12 +166,13 @@ function makeRelatedList( ytvid) {
 		$.getJSON( 'https://gdata.youtube.com/feeds/api/videos/' + ytvid + '/related', { 
 				v: 2,
 				'max-results': 5,
-				format: 5,
+				format: 10,
 				alt: 'jsonc'
 				}, function(d) {
-					for( var i = 0; i < 5; ++i) {
+					for( var i = 0; i < 10; ++i) {
 						var cvid = d.data.items[i].id;
 						$('#relatedlist').append( '<span class="candidate">' + cvid + '</span><br />');
+						/*
 						$.getJSON( 'https://gdata.youtube.com/feeds/api/videos/' + cvid + '/related', { 
 							v: 2,
 							'max-results': 2,
@@ -181,6 +182,7 @@ function makeRelatedList( ytvid) {
 								for( var i = 0; i < 2; ++i)
 							$('#relatedlist').append( '<span class="candidate">' + d.data.items[i].id + '</span><br />');
 							});
+						*/
 					}
 				});
 	//}
